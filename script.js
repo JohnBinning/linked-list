bf3056f3b8aeeebb43da499cb307ad5b85e1c028
John Binning <john.binning17@gmail.com>
2017-02-07 15:28:47 -0700

    updated javascript with correct title input var

--- script.js
+++ script.js
@@ -9,6 +9,9 @@
 // $('.read-btn').on('click', function() {
 //   $('.read-btn').toggleClass('.read');
 // })
+var titleInput = $('#title-input')
+// var urlInput = $('#url-input')
+
 
 $('#title-input, url-input').keyup(function(){
   $('#enter-btn').prop('disabled', false);
@@ -20,7 +23,16 @@ $('#enter-btn').on('click', function() {
 
 
 // will want to call makeLink in another secondFunction.  secondFunction will need to audit that the proper inputs have been made on click of enter button then call makeLink.
-function makeLink (){
+function makeLink (titleValue){
   $('.section2').prepend(
-    '<article>' + '<h2 class="card-title">' + "title" + '</h2>' + '<div class="card-url">' + "url" + '</div>' + '<button class="read-button">Read</button>' + '<button class="delete-button">Delete</button>' + '</article>');
+    '<article>' + '<h2 class="card-title">' + titleInput.val() + '</h2>' + '<div class="card-url">' + "url" + '</div>' + '<button class="read-button">Read</button>' + '<button class="delete-button">Delete</button>' + '</article>');
 };
+
+
+
+
+// make a function that disables the enter button based on if title || url = ""
+
+// make a function that calls the above function on each keyup titleinput.keyup(abovefunction)
+
+// call the second function on page load - globally
